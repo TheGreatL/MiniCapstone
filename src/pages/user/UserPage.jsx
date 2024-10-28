b8f8b8672719c2fc38bf29f9205e6635bca285e4
Ryuzaki <bpow2308@gmail.com>
2024-10-28 23:36:24 +0800

    fifth

--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function UserPage() {
-  return <main>UserPage</main>;
+  return (
+    <main>
+      UserPage
+      <Link to="/admin">Admin</Link>
+    </main>
+  );
 }
 
 export default UserPage;
